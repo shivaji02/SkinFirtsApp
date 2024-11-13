0deda5d4710a46c58a8cf4e70441cff35eabf83d
Govinda Biswas <govinda.biswas@neosoftmail.com>
2024-11-13 11:28:36 +0530

    fontStyle.tsx

--- src/screens/auth/loginScreen/styles.ts
+++ src/screens/auth/loginScreen/styles.ts
@@ -3,6 +3,8 @@ import { StyleSheet } from "react-native"
 export const useStyle =()=>{
 
     return StyleSheet.create({
-        
+        nain:{
+     
+        }
     })
 }
---
+++ src/theme/fontStyle.tsx
@@ -0,0 +1,24 @@
+import { COLORS } from "./colors";
+
+export const LeagueSpartanFont ={  
+    Thin:"LeagueSpartan-Thin",//100
+    ExtraLight:"LeagueSpartan-ExtraLight",//200
+    Light:"LeagueSpartan-Light",//300
+    Regular:"LeagueSpartan-Regular",//400
+    Medium:"LeagueSpartan-Medium",//500
+    SemiBold:"LeagueSpartan-SemiBold",//600
+    Bold:"LeagueSpartan-Bold",//700
+    ExtraBold:"LeagueSpartan-ExtraBold",//800
+    Black:"LeagueSpartan-Black",//900
+}
+
+export const FontStyle =({ color = COLORS.common.black, fontsize, fontFamily, lineHeight}: { color: string; fontsize?: number | undefined; fontFamily: LeagueSpartanFontFamily|string; lineHeight?:number})=>{
+    return{
+        color: color,
+        fontSize: fontsize,
+        fontFamily: typeof fontFamily === "string" ? fontFamily : LeagueSpartanFont[fontFamily as LeagueSpartanFontFamily],
+        ...(lineHeight && { lineHeight })
+    }
+}
+
+export type LeagueSpartanFontFamily = keyof typeof LeagueSpartanFont;
